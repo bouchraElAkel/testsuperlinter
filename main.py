140ba118851af3460082d0b1706d6e9817eed9ff
bouchraElAkel <117727916+bouchraElAkel@users.noreply.github.com>
2022-11-22 12:44:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def converter(x):
-  print(f"Decimal: {x} Binaire: {bin(x)} Hexadecimal: {hex(x)}")
+ def converter(x):
+    print(f"Decimal: {x} Binaire: {bin(x)} Hexadecimal: {hex(x)}")
+
+
 converter(10)
